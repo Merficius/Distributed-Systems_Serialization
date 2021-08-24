2f7b52ed42d9d509450df45d03390dd3acd3bec4
Mariano Portilla <chemanoportilla@hotmail.com>
2021-08-24 17:15:58 -0500

    Add basic rules for activity

---
+++ marshall.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Variables globales
+char c1 = 'A';
+char c2 = 'B';
+
+int i = 16;		// entero a 32 bits representado en LITTLE-ENDIAN
+
+char c3 = 'C';
+char c4 = 'D';
+
+int serializeData(void);
+
+int main() {
+    
+    serializeData();
+
+    return 0;
+}
+
+int serializeData() {
+
+    return 0;
+}
---
+++ unmarshall.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Variables globales
+char c1 = 'U';
+char c2 = 'U';
+
+long i = -1;		// entero a 64 bits representado en LITTLE-ENDIAN
+
+char c3 = 'U';
+char c4 = 'U';
+
+int readSerializedData(void);
+
+int main() {
+    
+    readSerializedData();
+
+    return 0;
+}
+
+int readSerializedData() {
+
+    return 0;
+}
