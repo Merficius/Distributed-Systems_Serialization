704cb1e616f7d3da7ff05cb5cb5ad2eac4fb5903
Mariano Portilla <chemanoportilla@hotmail.com>
2021-08-25 16:36:19 -0500

    Add vector representation to save characters

--- marshall.c
+++ marshall.c
@@ -22,25 +22,22 @@ int main()
 int serializeData()
 {
     // Write binary data
-    FILE* file = fopen("datos.bin", "wb"); // Creating the file
+    FILE* file = fopen("datos.mem", "wb"); // Creating the file
     char* p = &c1;
     
     //char *p2 = (char *) &i;
 
-    fprintf(file, "%d\n", *p);                
-    fprintf(file, "%d\n", *(p+1));                
+    fprintf(file, "%c\n", p[0]);                
+    fprintf(file, "%c\n", p[1]);                
     
-    fprintf(file, "%d\n", *(p+7));                
-    fprintf(file, "%d\n", *(p+6));                
-    fprintf(file, "%d\n", *(p+5));                
-    fprintf(file, "%d\n", *(p+4));                
+    fprintf(file, "%c\n", p[7]);                
+    fprintf(file, "%c\n", p[6]);                
+    fprintf(file, "%c\n", p[5]);                
+    fprintf(file, "%c\n", p[4]);                
 
-    fprintf(file, "%d\n", *(p+8)); 
-    fprintf(file, "%d\n", *(p+9)); 
-
-    //fprintf(file, "%d", *(p+2));                
-    //fprintf(file, "%d", *(p+3));                
-    //fprintf(file, "%d", c2);                
+    fprintf(file, "%c\n", p[8]); 
+    fprintf(file, "%c\n", p[9]); 
+          
     fclose(file);                          // Closing the file
     return 0;
 
