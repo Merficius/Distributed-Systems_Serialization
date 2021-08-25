5796c94535acb6d2a908f0e58b809bd36be07981
Mariano Portilla <chemanoportilla@hotmail.com>
2021-08-24 19:27:21 -0500

    Add functionality to read one character

--- unmarshall.c
+++ unmarshall.c
@@ -4,21 +4,29 @@
 char c1 = 'U';
 char c2 = 'U';
 
-long i = -1;		// entero a 64 bits representado en LITTLE-ENDIAN
+long i = -1; // entero a 64 bits representado en LITTLE-ENDIAN
 
 char c3 = 'U';
 char c4 = 'U';
 
 int readSerializedData(void);
 
-int main() {
-    
+int main()
+{
+
     readSerializedData();
 
     return 0;
 }
 
-int readSerializedData() {
+int readSerializedData()
+{
 
+    //Reading binary data
+    FILE* file = fopen("datos.bin", "rb");  
+    c1 = fgetc(file) + "" + fgetc(file);
+    fclose(file);
+
+    printf("%c", c1);
     return 0;
 }
